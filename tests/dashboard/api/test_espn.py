a5d0b652f5f57a8bd7cab1f0c5d23b2ec01f9291
PietroMondini <pietro.mondini03@gmail.com>
2026-05-21 12:50:55 +0200

    REFACTORING: adding test cases for standings structure

--- tests/dashboard/api/test_espn.py
+++ tests/dashboard/api/test_espn.py
@@ -7,12 +7,20 @@ def client():
 
 def test_get_standings(client):
     standings = client.get_standings()
+    east_standings = standings["children"][0]
+    west_standings = standings["children"][1]
+
+    # Check that there are two conferences
     assert len(standings["children"]) == 2
-    assert standings["children"][0]["name"] == "Eastern Conference"
-    assert standings["children"][1]["name"] == "Western Conference"
+
+    # Check that the names of the conferences are correct
+    assert east_standings["name"] == "Eastern Conference"
+    assert west_standings["name"] == "Western Conference"
+
+    # Check that there are 15 teams in each conference
+    assert len(east_standings["standings"]["entries"]) == 15
+    assert len(west_standings["standings"]["entries"]) == 15
 
 def test_get_teams(client):
     teams = client.get_teams()
     assert len(teams["sports"][0]["leagues"][0]["teams"])
-
-
