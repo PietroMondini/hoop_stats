44b7a08a382d58b2ee11f22130aa8df797ac1f01
PietroMondini <pietro.mondini03@gmail.com>
2026-05-21 12:43:44 +0200

    RED: test_get_teams

--- tests/dashboard/api/test_espn.py
+++ tests/dashboard/api/test_espn.py
@@ -6,7 +6,13 @@ def client():
     return ClientESPN()
 
 def test_get_standings(client):
-    assert len(client.get("standings")["children"]) == 2
-    assert client.get("standings")["children"][0]["name"] == "Eastern Conference"
-    assert client.get("standings")["children"][1]["name"] == "Western Conference"
+    standings = client.get_standings()
+    assert len(standings["children"]) == 2
+    assert standings["children"][0]["name"] == "Eastern Conference"
+    assert standings["children"][1]["name"] == "Western Conference"
+
+def test_get_teams(client):
+    teams = client.get_teams()
+    assert len(teams["sports"][0]["leagues"][0]["teams"])
+
 
